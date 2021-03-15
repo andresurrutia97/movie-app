ccc5a76c18526a9a2fe026c3506b5b26c858d664
Andres Urrutia <andresurrutia@MacBook-Pro.local>
2021-03-15 10:29:40 -0500

    Commented the Auth0 for login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+// import { useAuth0 } from '@auth0/auth0-react';
 
-import Spinner from './components/Ui/Spinner';
+// import Spinner from './components/Ui/Spinner';
 import ProtectedRoutes from './Routing/ProtectedRoutes';
-import UnprotectedRoutes from './Routing/UnprotectedRoutes';
+// import UnprotectedRoutes from './Routing/UnprotectedRoutes';
 
 const App = () => {
-  const { isAuthenticated, isLoading } = useAuth0();
+  // const { isAuthenticated, isLoading } = useAuth0();
 
   // const routes = isAuthenticated ? <ProtectedRoutes /> : <UnprotectedRoutes />;
   const routes = <ProtectedRoutes />;
 
-  return <>{isLoading ? <Spinner /> : routes}</>;
+  // return <>{isLoading ? <Spinner /> : routes}</>;
+  return <>{routes}</>;
 };
 
 export default App;
